30e3d6e542b26a6e49cc4671e63fbc7ce99d1b9e
Zange10 <niklaszan@protonmail.com>
2024-12-04 21:40:57 +0100

    solution day 01

--- sol_01/sol_01.go
+++ sol_01/sol_01.go
@@ -1,7 +1,71 @@
 package sol_01
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
 
 func RunSol01() {
-	fmt.Println("Test")
+
+	var left []int
+	var right []int
+
+	// open file
+	f, err := os.Open("./sol_01/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// remember to close the file at the end of the program
+	defer f.Close()
+
+	// read the file line by line using scanner
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		// do something with a line
+		temp := strings.Fields(scanner.Text())
+
+		templeft, _ := strconv.Atoi(temp[0])
+		tempright, _ := strconv.Atoi(temp[1])
+
+		left = append(left, templeft)
+		right = append(right, tempright)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	sort.Ints(left)
+
+	sort.Ints(right)
+
+	sum := 0
+
+	for i, _ := range left {
+		tempdiff := left[i] - right[i]
+		if tempdiff < 0 {
+			tempdiff = -tempdiff
+		}
+		sum += tempdiff
+	}
+	fmt.Printf("%d\n", sum)
+
+	sum = 0
+
+	for _, val_left := range left {
+		for _, val_right := range right {
+			if val_left == val_right {
+				sum += val_left
+			}
+		}
+	}
+
+	fmt.Printf("%d\n", sum)
+
 }
